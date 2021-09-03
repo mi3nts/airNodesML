b1de72c393311ad0ed1d4cce23d7dc587162d2fa
Lakitha Wijeratne <lhw150030@europa.circ.utdallas.edu>
2021-09-03 09:16:36 -0500

    V3 Updates

--- firmware/utdNodes/completeUpdate/completeUpdateLiveSlurm.sh
+++ firmware/utdNodes/completeUpdate/completeUpdateLiveSlurm.sh
@@ -7,7 +7,7 @@
 #SBATCH --mail-user=lhw150030@utdallas.edu     # Where to send mail	
 #SBATCH -N 1                  # Total number of nodes requested
 #SBATCH -n 16                 # Total number of mpi tasks requested
-#SBATCH --array=1-31          # Array ranks to run
+#SBATCH --array=3-3          # Array ranks to run
 #SBATCH -t 48:00:00           # Run time (hh:mm:ss) - 48 hours
 
 
